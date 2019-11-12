e01337335225bd3a20e58015fca9c54e94339325
Magda <mfryczka96@gmail.com>
2019-11-12 12:43:38 +0100

    creating employee model

--- Employees/models.py
+++ Employees/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    employee_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100,unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
