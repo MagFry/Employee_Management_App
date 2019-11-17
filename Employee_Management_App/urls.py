d53988047665a2082890afdd1f64a7fe6c7437ee
Magda <mfryczka96@gmail.com>
2019-11-17 17:43:04 +0100

    adding url - add employee page

--- Employee_Management_App/urls.py
+++ Employee_Management_App/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.urls import path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('', include('frontend.urls')),
     url('', include('Employees.urls')),
 ]
