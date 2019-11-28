d3cd071443f85fd5dfdad898df6b414a7ceb5308
Magda <mfryczka96@gmail.com>
2019-11-28 23:05:30 +0100

    adding required fields to Employees model

--- Employees/models.py
+++ Employees/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Employee(models.Model):
     employee_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100,unique=True)
+    name = models.CharField(max_length=100, blank=False)
+    surname = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100,unique=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
