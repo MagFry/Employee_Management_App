fdce0bc190652db31aaa853f2edd6a167e37a237
Magda <mfryczka96@gmail.com>
2019-11-29 23:59:39 +0100

    updating Employee model

--- Employees/models.py
+++ Employees/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Employee(models.Model):
     employee_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, blank=False)
     surname = models.CharField(max_length=100, blank=False)
     email = models.EmailField(max_length=100,unique=True, blank=False)
+    owner = models.ForeignKey(
+        User, related_name="employees", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
